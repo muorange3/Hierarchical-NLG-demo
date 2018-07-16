cdd4b4dcb3f4ae6a3676fd462b8aa71a152719f2
Mu Yu <muorange3@gmail.com>
2018-07-16 21:16:58 +0800

    change color

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def result():
         [['new','All Bar One','n'],['new','place','n'],['new','it','n'],['new','Midsummer House','n']],
         [['old','All Bar One','n'],['new','is','v'],['new','priced','v'],['old','place','n'],['old','it','n'],['new','is','v'],['new','called','v'],['old','Midsummer House','n']],
         [['old','All Bar One','n'],['old','is','v'],['new','moderately','a'],['old','priced','v'],['new','Italian','a'],['old','place','n'],['old','it','n'],['old','is','v'],['old','called','v'],['old','Midsummer House','n']],
-        [['new','Near','Other.'],['old','All Bar One','n'],['old','is','v'],['new','a','Other.'],['old','moderately','a'],['old','priced','v'],['old','Italian','a'],['old','place','n'],['old','it','n'],['old','is','v'],['old','called','v'],['old','Midsummer House','n']]
+        [['new','Near','other'],['old','All Bar One','n'],['old','is','v'],['new','a','other'],['old','moderately','a'],['old','priced','v'],['old','Italian','a'],['old','place','n'],['old','it','n'],['old','is','v'],['old','called','v'],['old','Midsummer House','n']]
     ]
     resultSent = 'Near All Bar One is a moderately priced Italian place it is called Midsummer House'
     return render_template('hnlg.html', resultList=resultList, resultSent=resultSent)
