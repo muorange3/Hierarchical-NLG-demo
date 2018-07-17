5c861192cef0d0d7eb67ae21501429375b2706f9
Mu Yu <muorange3@gmail.com>
2018-07-18 01:13:08 +0800

    tag update

--- main.py
+++ main.py
@@ -37,6 +37,13 @@ def result():
             if same==1:
                 resultList = example_pairs[i]['output']
                 resultSent = example_pairs[i]['output_sent']
+                if result['name']!='':
+                    resultSent = resultSent.replace('NAMETOKEN', result['name'][0])
+                    resultSent = resultSent.replace('NAME', result['name'][0])
+                    for l in resultList:
+                        for w in l:
+                            for i in range(len(w)):
+                                if w[i] in ['NAMETOKEN', 'NAME']: w[i] = result['name'][0]
 
     return render_template('hnlg.html', resultList=resultList, resultSent=resultSent, request=result)
 
