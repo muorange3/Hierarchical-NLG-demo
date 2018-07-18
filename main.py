fa2ce5f8610f5ae6a04a252652d28ee63c7c896f
Mu Yu <muorange3@gmail.com>
2018-07-18 09:08:41 +0800

    change word type

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-import json
+import json, os, sqlite3
 from flask import (
     Flask, jsonify, render_template, request, send_from_directory, url_for, 
     g, redirect, session,
 )
-import json, os, sqlite3
 app = Flask(__name__)
 
-with open('out.json','r') as f: example_pairs = json.load(f)
+SAMPLE = True
+
+def clean(d):
+    for k,v in d.items():
+        print(k,v)
+        if v[0]=='none': d[k]=''
+        else: d[k] = v[0]
+    return d
+
+def assign_word_type(word_layer):
+    word_type = ['n','v','a','other']
+    resultList = [[],[],[],[]]
+    for i in range(len(word_layer)):
+        if i==0:
+            for w in word_layer[i]: resultList[i].append([w,word_type[i]])
+        else:
+            for w in word_layer[i]:
+                if w in word_layer[i-1]: resultList[i].append(resultList[i-1][word_layer[i-1].index(w)])
+                else: resultList[i].append([w,word_type[i]])
+    print(resultList)
+    return resultList
+
+with open('out3.json','r') as f: example_pairs = json.load(f)
 
 @app.route('/')
 def main():
@@ -17,34 +38,36 @@ def main():
 @app.route('/result', methods=['GET', 'POST'])
 def result():
     if request.method == 'POST':
-        result = request.form.to_dict(flat=False)       # user request
+        result = clean(request.form.to_dict(flat=False))       # user request
         print(result)
 
-        resultList = [      # generated words
-            [['new','All Bar One','n'],['new','place','n'],['new','it','n'],['new','Midsummer House','n']],
-            [['old','All Bar One','n'],['new','is','v'],['new','priced','v'],['old','place','n'],['old','it','n'],['new','is','v'],['new','called','v'],['old','Midsummer House','n']],
-            [['old','All Bar One','n'],['old','is','v'],['new','moderately','a'],['old','priced','v'],['new','Italian','a'],['old','place','n'],['old','it','n'],['old','is','v'],['old','called','v'],['old','Midsummer House','n']],
-            [['new','Near','other'],['old','All Bar One','n'],['old','is','v'],['new','a','other'],['old','moderately','a'],['old','priced','v'],['old','Italian','a'],['old','place','n'],['old','it','n'],['old','is','v'],['old','called','v'],['old','Midsummer House','n']]
-        ]
-        resultSent = 'Near All Bar One is a moderately priced Italian place it is called Midsummer House'
-        for i in range(len(example_pairs)):
-            same = 1
-            for k,v in result.items():
-                if example_pairs[i]['input'][k] in ['','NAMETOKEN']: continue
-                if v[0] != example_pairs[i]['input'][k]:
-                    same=0
-                    break
-            if same==1:
-                resultList = example_pairs[i]['output']
-                resultSent = example_pairs[i]['output_sent']
-                if result['name']!='':
-                    resultSent = resultSent.replace('NAMETOKEN', result['name'][0])
-                    for l in resultList:
-                        for w in l:
-                            for i in range(len(w)):
-                                if w[i]=='NAMETOKEN': w[i] = result['name'][0]
+        if SAMPLE:
+            resultList = [          # example
+                ['All Bar One','place','it','Midsummer House'],
+                ['All Bar One','is''priced','place','it','is','called','Midsummer House'],
+                ['All Bar One','is','moderately','priced','Italian','place','it','is','called','Midsummer House'],
+                ['Near','All Bar One','is','a','moderately','priced','Italian','place','it','is','called','Midsummer House']
+            ]
+            resultSent = 'Near All Bar One is a moderately priced Italian place it is called Midsummer House'
 
-    return render_template('hnlg.html', resultList=resultList, resultSent=resultSent, request=result)
+            for example_pair in example_pairs:
+                same = 1
+                for k,v in result.items():
+                    if example_pair['input'][k] in ['','NAME']: continue
+                    if v != example_pair['input'][k]:
+                        same=0
+                        print(k,v,example_pair['input'])
+                        break
+                if same==1:
+                    resultList = example_pair['output']
+                    resultSent = example_pair['output_sent']
+                    if result['name']!='':
+                        resultSent = resultSent.replace('NAMETOKEN', result['name'])
+                        for l in resultList:
+                            for i,w in enumerate(l):
+                                if w=='NAMETOKEN': l[i] = result['name']
+        # else: resultList = model_generated_list
+    return render_template('hnlg.html', resultList=assign_word_type(resultList), resultSent=resultSent, request=result)
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=7777)
